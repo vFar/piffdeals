d1f4cbdc958a43d894837b36424b8e78eafe021f
Markuss Balodis <markussdove@gmail.com>
2024-06-08 00:15:24 +0300

    Merge branch 'main' of https://github.com/vFar/piffdeals

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -6,11 +6,7 @@ import { Link } from '@inertiajs/vue3';
 <template>
     <div
         class="cross-patternSVGLight bg-whiter min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-        <div>
-            <Link href="/">
-            <ApplicationLogo class="w-20 h-20 fill-current text-red-600" />
-            </Link>
-        </div>
+
 
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             <slot />
